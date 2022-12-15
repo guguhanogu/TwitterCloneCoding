4ce1da3b507b93efaf3fe0175363f5f3aed1eb74
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2022-12-16 00:40:18 +0900

    Init: Init Project
    Init: Init Project

---
+++ TwitterClone/TwitterClone/View/MessageView/MessageView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageView.swift
+//  TwitterClone
+//
+//  Created by 최한호 on 2022/12/15.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageView()
+    }
+}
